6007a9083930530bc6f47fb9cedccdd1869d97bc
Rimantas <belauzas@gmail.com>
2024-08-20 09:52:29 +0300

    Math

---
+++ js/method/math.js
@@ -0,0 +1,52 @@
+console.log(Math);
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.SQRT2 * Math.SQRT2);
+
+console.log(Math.abs(5));
+console.log(Math.abs(-5));
+console.log(Math.abs(0));
+
+console.log(Math.cbrt(40) * Math.cbrt(40) * Math.cbrt(40));
+
+console.log(Math.fround(2));
+console.log(Math.fround(-2));
+console.log(Math.fround(2.5));
+console.log(Math.fround(2.5148562154));
+
+console.log(Math.hypot(3, 4));
+
+console.log(Math.imul(3, 4));
+console.log(Math.imul(300, 400));
+
+console.log(Math.pow(0, 0));
+console.log(Math.pow(2, 0));
+console.log(Math.pow(2, 1));
+console.log(Math.pow(2, 2));
+console.log(Math.pow(2, 3));
+console.log(Math.pow(2, 4));
+console.log(Math.pow(2, 5));
+
+console.log(Math.sign(5));
+console.log(Math.sign(5548562));
+console.log(Math.sign(0));
+
+console.log(Math.sign(-5));
+console.log(Math.sign(-5548562));
+console.log(Math.sign(-0));
+
+console.log(Math.sqrt(4));
+console.log(Math.sqrt(9));
+console.log(Math.sqrt(2));
+
+
+// console.log(Math.ceil);
+// console.log(Math.floor);
+// console.log(Math.round);
+// console.log(Math.trunc);
+
+// console.log(Math.max);
+// console.log(Math.min);
+
+// console.log(Math.random);
